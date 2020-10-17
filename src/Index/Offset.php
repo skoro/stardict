40f782bad63c4d7a917954502c455943d7c78690
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-10-17 16:21:39 +0300

    Binary index lookup implementation.

---
+++ src/Index/Offset.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace StarDict\Index;
+
+class Offset
+{
+    private string $query;
+    private int $offset;
+    private int $length;
+
+    public function __construct(string $query, int $offset, int $length)
+    {
+        $this->query = $query;
+        $this->offset = $offset;
+        $this->length = $length;
+    }
+
+    public function getQuery(): string
+    {
+        return $this->query;
+    }
+
+    public function getOffset(): int
+    {
+        return $this->offset;
+    }
+
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+}
