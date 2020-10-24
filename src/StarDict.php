7dc76c14425c85ea0a0c43e7bc9c8a997426c6c2
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-10-24 17:12:21 +0300

    StarDict builder.

--- src/StarDict.php
+++ src/StarDict.php
@@ -3,8 +3,15 @@
 namespace StarDict;
 
 use RuntimeException;
-use StarDict\Index\BinaryIndexLookup;
-use StarDict\Index\IndexLookup;
+use StarDict\DictData\DataReader;
+use StarDict\DictData\FileDataReader;
+use StarDict\DictData\Sequences\{
+    PhoneticString, PngFile, PureText, TypeSequence, WavFile,
+};
+use StarDict\DictData\TypeSequenceManager;
+use StarDict\Index\DataOffsetItem;
+use StarDict\Index\FileIndexProvider;
+use StarDict\Index\IndexDataHandler;
 use StarDict\Info\DictInfoFile;
 use StarDict\Info\DictProvider;
 use StarDict\Info\SignatureChecker;
@@ -12,30 +19,69 @@ use StarDict\Info\SignatureChecker;
 class StarDict
 {
     private Dict $dict;
-    private IndexLookup $indexLookup;
+    private IndexDataHandler $indexHandler;
+    private DataReader $dataReader;
+
+    /**
+     * @var DataOffsetItem[]
+     */
+    private array $offsets;
+
+    /**
+     * @var TypeSequence[]
+     */
+    private array $typeSequences;
 
     public function __construct(
         Dict $dict,
-        IndexLookup $indexLookup
+        IndexDataHandler $indexHandler,
+        DataReader $dataReader,
+        TypeSequenceManager $typeSequenceManager
     ) {
         $this->dict = $dict;
-        $this->indexLookup = $indexLookup;
+        $this->indexHandler = $indexHandler;
+        $this->dataReader = $dataReader;
+        $this->offsets = [];
+        $this->typeSequences = $typeSequenceManager->getSequences($dict->getSameTypeSequence());
+
         $this->checkVersion();
+        $this->buildOffsets();
     }
 
-    protected function checkVersion()
+    protected function checkVersion(): void
     {
         if ($this->dict->getVersion() !== '2.4.2') {
             throw new RuntimeException('Only 2.4.2 version is supported.');
         }
     }
 
-    public static function createFromFile(string $filename): self
+    protected function buildOffsets(): void
     {
-        $dict = static::createDictProviderFromFile($filename)->getDict();
+        foreach ($this->indexHandler->getDataOffsets() as $index => $offset) {
+            $this->offsets[$index] = $offset;
+        }
+    }
+
+    public function get(string $toc)
+    {
+        $offset = $this->offsets[$toc];
+        $this->dataReader->readFromOffset($offset, $this->typeSequences);
+    }
+
+    public static function createFromFiles(
+        string $fileIfo,
+        string $fileIdx,
+        string $fileDict
+    ): self {
+        $dict = static::createDictProviderFromFile($fileIfo)->getDict();
+        $index = new FileIndexProvider($fileIdx, $dict->getIndexFilesize());
+        $reader = new FileDataReader($fileDict);
+
         return new static(
             $dict,
-            new BinaryIndexLookup($filename, $dict)
+            $index->getIndexDataHandler(),
+            $reader,
+            static::createTypeSequenceManager()
         );
     }
 
@@ -48,5 +94,15 @@ class StarDict
     public function getDict(): Dict
     {
         return $this->dict;
+    }
+
+    public static function createTypeSequenceManager(): TypeSequenceManager
+    {
+        return (new TypeSequenceManager())
+            ->register(new PureText())
+            ->register(new PhoneticString())
+            ->register(new PngFile())
+            ->register(new WavFile())
+            ;
     }
 }
